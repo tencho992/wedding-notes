a1de5cc66372779dccdc30e71bb9e2b1e6680801
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-04-26 11:13:42 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://tenzinc:tenzinc@burn-notice.ofi7dnh.mongodb.net/weddingnotes?retryWrites=true&w=majority', 
+    'dbName': 'weddingnotes'
+};
